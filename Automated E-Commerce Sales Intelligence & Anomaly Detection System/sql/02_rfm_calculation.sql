4851011f9f46ac74580244c0417c2e1c96282446
Keerthanagr12 <167233450+Keerthanagr12@users.noreply.github.com>
2026-01-18 21:33:53 +0530

    Delete Automated E-Commerce Sales Intelligence & Anomaly Detection System directory
    Remove nested project folder

--- Automated E-Commerce Sales Intelligence & Anomaly Detection System/sql/02_rfm_calculation.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW rfm_base AS
-SELECT
-    CustomerID,
-    MAX(InvoiceDate) AS last_purchase_date,
-    DATEDIFF(CURRENT_DATE, MAX(InvoiceDate)) AS recency,
-    COUNT(DISTINCT InvoiceNo) AS frequency,
-    SUM(line_revenue) AS monetary
-FROM clean_transactions
-GROUP BY CustomerID;
